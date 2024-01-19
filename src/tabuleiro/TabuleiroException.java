b5d3557b255fe7fa3562a879b8ffc49c25a7aff3
David de Sousa <david86_3@hotmail.com>
2024-01-19 09:24:17 -0300

    TabuleiroException e Programação defensiva

---
+++ src/tabuleiro/TabuleiroException.java
@@ -0,0 +1,13 @@
+package tabuleiro;
+
+public class TabuleiroException extends RuntimeException{
+
+		private static final long serialVersionUID = 1L;
+		
+		public TabuleiroException(String msg) {
+			super(msg);
+		}
+		
+	
+
+}
